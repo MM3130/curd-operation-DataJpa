f1818d35a506fbea9447860052b3186d74bad566
Manjunath Mulimani <mulimani338@gmail.com>
2024-10-10 11:15:24 +0530

    initially project start

---
+++ src/main/java/in/mm/main/CurdOperationDataJpaApplication.java
@@ -0,0 +1,13 @@
+package in.mm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOperationDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurdOperationDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/mm/main/CurdOperationDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mm.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOperationDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
