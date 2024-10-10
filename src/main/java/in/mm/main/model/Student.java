b324df13f67a629cc5032c2ff85d722dab95036f
Manjunath Mulimani <mulimani338@gmail.com>
2024-10-10 14:10:10 +0530

    model class added

---
+++ src/main/java/in/mm/main/model/Student.java
@@ -0,0 +1,13 @@
+package in.mm.main.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+}
