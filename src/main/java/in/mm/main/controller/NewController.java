d90ec9e76a10e08e93b1c178ff789314b2470b23
Manjunath Mulimani <mulimani338@gmail.com>
2024-10-10 15:23:36 +0530

    add code into new controler

--- src/main/java/in/mm/main/controller/NewController.java
+++ src/main/java/in/mm/main/controller/NewController.java
@@ -4,5 +4,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class NewController {
+    public String  NewController() {
+        return "NewController";
+    }
 
+    public String  NewController2() {
+        return "NewController2";
+    }
 }
